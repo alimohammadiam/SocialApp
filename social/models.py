8a95889bb36b1a473ee659928e2aa1b4153bb9ee
Ali Mohammadi <ali0182mohammadi@gmail.com>
2024-08-01 03:05:39 +0330

    Personalization Django User with "AbstractUser" & Create Model "User" & Settings for Images & CreateSuperUser

--- social/models.py
+++ social/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name='تاریخ تولد')
+    bio = models.TextField(blank=True, null=True, verbose_name='بایو')
+    photo = models.ImageField(upload_to='account_images/', blank=True, null=True, verbose_name='تصویر')
+    job = models.CharField(max_length=250, blank=True, null=True, verbose_name='شغل')
+    phone = models.CharField(max_length=11, blank=True, null=True, verbose_name='شماره تلفن')
